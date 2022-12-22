8a0950fd53f247282406893497a208f921359951
cuiyong <anke1938@qq.com>
2022-12-22 13:37:25 +0800

    add workflow_project.go & project.go

---
+++ examples/test_projectList.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/joker-bai/go-zadig"
+	"log"
+)
+
+func main() {
+
+	client, err := zadig.NewClient(
+		//"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiY3VpeW9uZyIsImVtYWlsIjoiY3VpeW9uZ0BuYW5jYWwuY29tIiwidWlkIjoiY2Y3YjRhN2MtNWE5MS0xMWVkLThlNTktNDZkYTQzOTY0YTUwIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiY3VpeW9uZyIsImZlZGVyYXRlZF9jbGFpbXMiOnsiY29ubmVjdG9yX2lkIjoibGRhcCIsInVzZXJfaWQiOiJjdWl5b25nIn0sImF1ZCI6InphZGlnIiwiZXhwIjo0ODI1MDI3ODUyfQ.cNnMqOQ7pKSNLVvKN2tLfbFKFn4pI7aMSH8691Unwx0",
+		"token",
+		zadig.WithBaseURL("https://xx.xx.com"))
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	projectList, r, err := client.Project.GetProjectList(&zadig.GetProjectListOptions{
+		//Verbosity: "detailed",
+		//Verbosity: "summary",
+		Verbosity: "minimal",
+	})
+	if err != nil {
+		return
+	}
+
+	fmt.Println(projectList)
+
+	fmt.Println(r)
+
+}
---
+++ project.go
@@ -0,0 +1,50 @@
+package zadig
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// 接口: 项目列表
+
+type ProjectService struct {
+	client *Client
+}
+
+type ProjectListResponse []struct {
+	Name       string   `json:"name"`
+	Envs       []string `json:"envs"`
+	Alias      string   `json:"alias"`
+	Desc       string   `json:"desc"`
+	UpdatedAt  int      `json:"updatedAt"`
+	UpdatedBy  string   `json:"updatedBy"`
+	Onboard    bool     `json:"onboard"`
+	Public     bool     `json:"public"`
+	DeployType string   `json:"deployType"`
+}
+
+// 请求数据
+type GetProjectListOptions struct {
+	Verbosity string `json:"Verbosity,omitempty"`
+}
+
+// GetProjectList  https://leyancd.nancalcloud.com/api/v1/picket/projects?verbosity=detailed
+func (p *ProjectService) GetProjectList(opt *GetProjectListOptions, options ...RequestOptionFunc) (*ProjectListResponse, *Response, error) {
+
+	//path := "/api/aslan/workflow/workflow?projectName=" + opt.PorjectName
+	path := fmt.Sprintf("api/v1/picket/projects?verbosity=%s", opt.Verbosity)
+
+	req, err := p.client.NewRequest(http.MethodGet, path, nil, options)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	projectList := new(ProjectListResponse)
+	resp, err := p.client.Do(req, &projectList)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return projectList, resp, err
+}
