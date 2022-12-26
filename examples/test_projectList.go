376739b94676b5007d8a4c4ea9ca8bef1efee56a
cuiyong <anke1938@qq.com>
2022-12-26 17:36:35 +0800

    add workflow_project.go & project.go

--- examples/test_projectList.go
+++ examples/test_projectList.go
@@ -9,8 +9,7 @@ import (
 func main() {
 
 	client, err := zadig.NewClient(
-		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiY3VpeW9uZyIsImVtYWlsIjoiY3VpeW9uZ0BuYW5jYWwuY29tIiwidWlkIjoiY2Y3YjRhN2MtNWE5MS0xMWVkLThlNTktNDZkYTQzOTY0YTUwIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiY3VpeW9uZyIsImZlZGVyYXRlZF9jbGFpbXMiOnsiY29ubmVjdG9yX2lkIjoibGRhcCIsInVzZXJfaWQiOiJjdWl5b25nIn0sImF1ZCI6InphZGlnIiwiZXhwIjo0ODI1MDI3ODUyfQ.cNnMqOQ7pKSNLVvKN2tLfbFKFn4pI7aMSH8691Unwx0",
-		zadig.WithBaseURL("https://leyancd.nancalcloud.com"))
+		"token", zadig.WithBaseURL("https://xx.com"))
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
 	}
